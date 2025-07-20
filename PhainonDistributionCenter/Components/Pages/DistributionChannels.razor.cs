e77875a66c37aff5fd0ae45d02b8281b593bf252
WRC <hello_wrc@outlook.com>
2025-07-20 12:09:57 +0800

    feat: 实现大版本管理页面

--- PhainonDistributionCenter/Components/Pages/DistributionChannels.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.FluentUI.AspNetCore.Components;
-
-namespace PhainonDistributionCenter.Components.Pages;
-
-public partial class DistributionChannels : ComponentBase
-{
-
-}
