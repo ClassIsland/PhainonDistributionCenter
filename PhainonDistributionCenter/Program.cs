9de926af5141856dab44bb0385f890c3b721be63
wrc <hello_wrc@outlook.com>
2026-03-21 10:10:11 +0800

    fix: 修复 cdn 转发相关问题

--- PhainonDistributionCenter/Program.cs
+++ PhainonDistributionCenter/Program.cs
@@ -188,14 +188,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseForwardedHeaders(new ForwardedHeadersOptions()
+    var forwardedHeadersOptions = new ForwardedHeadersOptions
     {
-        ForwardedHeaders = ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedFor,
-        KnownProxies =
-        {
-            IPAddress.Parse("127.0.0.1")
-        }
-    });
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    };
+    forwardedHeadersOptions.KnownNetworks.Clear();
+    forwardedHeadersOptions.KnownProxies.Clear();
+    app.UseForwardedHeaders(forwardedHeadersOptions);
 }
 
 app.UseHttpsRedirection();
